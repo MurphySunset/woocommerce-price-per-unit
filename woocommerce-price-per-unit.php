f3b022c0587131852de0ed4b33c297c6c1fbee50
MurphySunset <baptiste.mourey@pm.me>
2021-01-28 11:41:42 +0100

    v0.2

--- woocommerce-price-per-unit.php
+++ woocommerce-price-per-unit.php
@@ -5,7 +5,8 @@
  * Works with variable products and on sale products.
  * Requires WooCommerce and Meta Box extensions. Please adapt source code to your needs.
  * Author: Baptiste Mourey
- * Version: 0.1
+ * Version: 0.2
+ * Version 0.2: moved from div to p.
  * Version 0.1: first commit.
  */
 
@@ -15,7 +16,7 @@ add_filter('woocommerce_get_price_html', 'price_per_unit');
 
 function price_per_unit($price) {
 
-    global  $woocommerce;
+    global $woocommerce;
     global $product;
 
     $post = get_the_id();
@@ -34,19 +35,19 @@ function price_per_unit($price) {
                 $variation_regular_price = $product->get_variation_regular_price();
 
                 if ($product->is_on_sale()) {
-                    return $price . '<div class="price-per-unit"><del>' . number_format($variation_regular_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</del>
-                    <ins>' . number_format($variation_min_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></div>' ;
+                    return $price . '<p class="price-per-unit"><del>' . number_format($variation_regular_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</del>
+                    <ins>' . number_format($variation_min_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></p>' ;
                 }
                 else {
-                    return $price . '<div class="price-per-unit"><ins>' . number_format($variation_min_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></div>' ;
+                    return $price . '<p class="price-per-unit"><ins>' . number_format($variation_min_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></p>' ;
                 }
             } else {
                 if ($product->is_on_sale()) {
-                    return $price . '<div class="price-per-unit"><del>' . number_format($regular_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</del>
-                    <ins>' . number_format($sale_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></div>' ;
+                    return $price . '<p class="price-per-unit"><del>' . number_format($regular_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</del>
+                    <ins>' . number_format($sale_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></p>' ;
                 }
                 else {
-                    return $price . '<div class="price-per-unit"><ins>' . number_format($regular_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></div>' ;
+                    return $price . '<p class="price-per-unit"><ins>' . number_format($regular_price / $coefficient, 2, ",", " "). $currency_symbol . ' / ' . $unit . '</ins></p>' ;
                 }
             }
             
@@ -63,9 +64,10 @@ function price_per_unit($price) {
 add_filter( 'rwmb_meta_boxes', 'price_per_unit_meta_box' );
 
 function price_per_unit_meta_box( $meta_boxes ) {
-    $prefix = '';
-    $unit = get_post_meta($post, 'kw_UNITE', true);
-    $coefficient = get_post_meta($post, 'kw_COEFF_UNITE', true);
+
+    $post = get_the_id();
+    $unit = get_post_meta($post, 'kw_UNITE', true); // 'kw_UNITE' is the name of your custom field for the unit.
+    $coefficient = get_post_meta($post, 'kw_COEFF_UNITE', true); // 'kw_COEFF_UNITE' is the name of your custom field for the unit.
 
     $meta_boxes[] = [
         'title'      => esc_html__( 'Prix par unité', 'online-generator' ),
@@ -78,13 +80,13 @@ function price_per_unit_meta_box( $meta_boxes ) {
                 'name' => esc_html__( 'Coefficient (poids net)', 'online-generator' ),
                 'desc'    => esc_html__( 'Doit être un chiffre avec un "." à la place de la virgule. Ex pour 100g : 0.100', 'online-generator' ),
                 'placeholder' => esc_html__( '0.100', 'online-generator' ),
-                'id'   => $prefix . 'kw_COEFF_UNITE',
+                'id'   => 'kw_COEFF_UNITE', // 'kw_COEFF_UNITE' is the name of your custom field for the unit.
             ],
             [
                 'type' => 'text',
                 'name' => esc_html__( 'Unité (kg, l, unité)', 'online-generator' ),
                 'placeholder' => esc_html__( 'kg ou l ou unité', 'online-generator' ),
-                'id'   => $prefix . 'kw_UNITE',
+                'id'   => 'kw_UNITE', // 'kw_UNITE' is the name of your custom field for the unit.
             ],
             [
                 'type' => 'heading',
